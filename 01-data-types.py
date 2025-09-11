21c93b48f09a661ac3e3fea30163e983db174009
Kael Guerrero <kaelguerrero22@gmail.com>
2025-09-11 10:54:29 -0400

    almost done lalala

---
+++ 01-data-types.py
@@ -0,0 +1,81 @@
+"""
+TASK 1:
+Create a float variable, and then convert it to an integer
+Print both the original variable and the converted integer.
+
+"""
+a = 3.0
+b = int(a)
+print(b)
+
+# Made the variable, "a" as a float variable. Then used another variable, "b" to convert the floating variable to an integer.
+# Once done I print NOT the variable A, but the variable B
+
+"""
+TASK 2:
+Write code that takes a number as input and prints whether 
+it's positive, negative, or zero using if-elif-else statements.
+"""
+
+a = 0
+if a < 0:
+    print("negative")
+elif a > 0:
+    print("positive")
+else:
+    print("Number is zero")
+
+# Made one variable, "a". Used if, elif, and else to determine if the variable is positive, negative, or 0.
+
+"""
+TASK 3:
+
+Write code that takes two numbers as input (an integer and a float), 
+performs addition, subtraction, multiplication, and division, and prints the results.
+"""
+
+a = 2
+b = 4.0
+print(a + b)
+
+# Created two variables, then printed these variables adding up for the result.
+
+"""
+TASK 4:
+
+Create a dictionary with keys as fruit names and values as their respective quantities. 
+Then print out the quantity of one of the fruits.
+"""
+
+Dico = {
+    'orange': 1,
+    'grapes': 2
+}
+print(Dico["grapes"])
+
+# Made a dictonary with two variables, with given name and number. Then printed out the string name using the bracket so the dictonary can work
+
+"""
+TASK 5:
+
+Create a string variable with that is a list of numbers and convert that string into a tuple.
+Then print out the both the original string and tuple.
+"""
+x = "1,2,3"
+y = x.split(",")
+print(x)
+print(y)
+
+# Created two variables. Even used the split command and printed out both results of x and y
+
+
+"""
+TASK 6:
+
+Create a list of your favorite subjects (as strings). 
+Use the join() function to combine all subjects into a single string separated by commas.
+Then create another version using " - " as the separator.
+Print both the original list and both joined strings.
+"""
+
+x = []
