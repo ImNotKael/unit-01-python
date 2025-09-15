8d1745fded7bc400f14a18c674cfdc762af54cbe
Kael Guerrero <kaelguerrero22@gmail.com>
2025-09-15 10:53:04 -0400

    Almost finished blitz task

---
+++ 03_list_blitz.py
@@ -0,0 +1,92 @@
+"""
+Task 1: Create a list
+Create a list with 4 elements and print it.
+"""
+
+list = ["Fire" , "Water" , "Earth" , "Wind"]
+print()
+print("TASK 1")
+print(list)
+print()
+
+"""
+Task 2: Add Element to a List
+Add an element to the end of the list. Print the updated 
+list.
+"""
+
+list = []
+list.append("Gravity")
+print()
+print("TASK 2")
+print(list)
+list.append("Lightning")
+print(list)
+print()
+
+"""
+Task 3: Remove Element from a List
+Remove a specific element from the list. Print the 
+updated list.
+"""
+
+list = ["fire" , "water"]
+print()
+print("TASK 3")
+print(list)
+list.remove("water")
+print(list)
+print()
+
+"""
+Task 4: Modify Element in a List
+Modify an element at a specific index with a new value. 
+Print the updated list.
+"""
+
+list = ["Samsung" , "Apple" , "Microsoft"]
+print()
+print("TASK 4")
+print(list)
+print(list[1])
+
+"""
+Task 5: Append Multiple Elements to a List
+Append multiple elements to the end of the list. Print 
+the updated list.
+"""
+
+list = []
+list.append("Pen")
+print()
+print("TASK 5")
+print(list)
+list.append("Pencil")
+list.append("Eraser")
+print(list)
+print()
+
+"""
+Task 6: Delete Element at a Specific Index
+Delete an element at a specific index. Print the updated 
+list.
+"""
+
+list = ["Samsung" , "Apple" , "Microsoft"]
+print()
+print("TASK 4")
+print(list)
+del list[1]
+print(list)
+
+"""
+Task 7: Slicing lists
+Create a new variable equal to the first 2 items of your list
+Then print out the new variable
+"""
+
+"""
+Task 8: Extend a List
+Extend the list with the elements of another list. Print 
+the updated list.
+"""
