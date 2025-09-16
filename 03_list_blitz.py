e7f6e7cd286ed704729fc170ed7acf1962e640a0
Kael Guerrero <kaelguerrero22@gmail.com>
2025-09-16 10:22:40 -0400

    late fix

--- 03_list_blitz.py
+++ 03_list_blitz.py
@@ -3,6 +3,7 @@ Task 1: Create a list
 Create a list with 4 elements and print it.
 """
 
+# Created variable, then printed out that variable
 list = ["Fire" , "Water" , "Earth" , "Wind"]
 print()
 print("TASK 1")
@@ -15,12 +16,15 @@ Add an element to the end of the list. Print the updated
 list.
 """
 
+# Created a list with nothing in it.
 list = []
-list.append("Gravity")
+# Added a string on that list. Then printed out that first list
+list.append("gravity")
 print()
 print("TASK 2")
 print(list)
-list.append("Lightning")
+# Added another string on the end of the list by doing .append . Then printed it out
+list.append("lightning")
 print(list)
 print()
 
@@ -30,10 +34,12 @@ Remove a specific element from the list. Print the
 updated list.
 """
 
+# Created a new list then printed out.
 list = ["fire" , "water"]
 print()
 print("TASK 3")
 print(list)
+# Used .remove and erased water to only print out fire.
 list.remove("water")
 print(list)
 print()
@@ -44,10 +50,12 @@ Modify an element at a specific index with a new value.
 Print the updated list.
 """
 
+# Created a new list, then printed it out
 list = ["Samsung" , "Apple" , "Microsoft"]
 print()
 print("TASK 4")
 print(list)
+# Printed the same variable, but instead added [1], which only focus on index 1, which is "Apple"
 print(list[1])
 
 """
@@ -56,11 +64,13 @@ Append multiple elements to the end of the list. Print
 the updated list.
 """
 
+# Created a new list. Added an append, then printed it out.
 list = []
 list.append("Pen")
 print()
 print("TASK 5")
 print(list)
+# Created two different appends, then printed out the new result.
 list.append("Pencil")
 list.append("Eraser")
 print(list)
@@ -72,10 +82,12 @@ Delete an element at a specific index. Print the updated
 list.
 """
 
-list = ["Samsung" , "Apple" , "Microsoft"]
+# Created a list with three indexes
+list = ["samsung" , "apple" , "microsoft"]
 print()
-print("TASK 4")
+print("TASK 6")
 print(list)
+# Used the del function and deleted index 1, which is "apple". Printed out the new result
 del list[1]
 print(list)
 
@@ -85,8 +97,26 @@ Create a new variable equal to the first 2 items of your list
 Then print out the new variable
 """
 
+# Created a list with four types of indexes. Then printed it out
+list = ["red" , "orange" , "yellow" , "green"]
+print()
+print("TASK 7")
+print(list)
+# Printed out the variable "list" again, but instead, added a subset.
+print(list[1:3])
+
 """
 Task 8: Extend a List
 Extend the list with the elements of another list. Print 
 the updated list.
 """
+
+# Created two lists, list and list2. Then printed both lists out
+list = ["mrforlenza" , "carlos"]
+list2 = ["msakhter" , "mrq" , "msmaria"]
+print()
+print("TASK 8")
+print(list)
+print(list2)
+# Combined both lists, then printed the result one of huge list
+print(list + list2)
