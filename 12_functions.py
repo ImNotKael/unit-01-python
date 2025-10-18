b82ef76440d97520902025db8af1db7867eb57dc
ImNotKael <kaelguerrero22@gmail.com>
2025-10-17 22:50:04 -0400

    Functions is done.

--- 12_functions.py
+++ 12_functions.py
@@ -6,10 +6,18 @@ agrument and prints a greeting message like "Hello, [name]!".
 
 print()
 print("TASK 1")
+"""
+Used a function that prints out the greeting mentioned
+"""
 def hellothing(name):
     print(f"Hello {name}")
 
+"""
+Then gave the function the variable name for "name"
+"""
 hellothing("Kael")
+
+# ------------------------------------------------------------
 
 """
 Task 2: Square of a Number
@@ -18,12 +26,22 @@ Write a function that takes an integer as an argument and returns its square.
 
 print()
 print("TASK 2")
+"""
+Created a function that squares a number.
+"""
 def square(a, b):
+    """
+    Used the return statement to return the squared number.
+    """
     return a ** b
 
+"""
+Created a variable for the function with a number to be squared. Printed "x" afterwards
+"""
 x = square(5, 2)
 print(x)
 
+# ------------------------------------------------------------
 
 """
 Task 3: Even or Odd
@@ -33,11 +51,23 @@ returns `True` if the number is even, and `False` otherwise.
 
 print()
 print("TASK 3")
+"""
+Created a function that deternmines if a number is even or odd.
+"""
 def evenorodd(x):
+    """
+    Used if statement to check if the number is even or odd.
+    """
     if x % 2 == 0:
+        """
+        Used return statement for True if even. Then False if odd.
+        """
         return True
     else:
         return False
+"""
+Created a variable for the function with a random number to be checked. Printed "x" afterwards
+"""
 x = evenorodd(4)
 print(x)
 
@@ -46,12 +76,23 @@ Task 4: Area of a Rectangle
 Write a function that takes the length and width of a rectangle and returns its area.
 """
 
+# ------------------------------------------------------------
+
 print()
 print("TASK 4")
-def square(a, b, c):
-    return a * b * c
+"""
+Created a function to calculate length times width to get area. (length * width)
+"""
+def square(a, b,):
+    """
+    Used return statement to calculate area by multiplying length and width.
+    """
+    return a * b
 
-x = square(4, 7, 3)
+"""
+Gave a random length and width for the variable printed out with the area.
+"""
+x = square(4, 7)
 print(x)
 
 """
@@ -60,12 +101,25 @@ Write a function that takes a temperature in Celsius and returns
 the equivalent temperature in Fahrenheit using the correct formula
 """
 
+# ------------------------------------------------------------
+
 print()
 print("TASK 5")
+"""
+Created a fuunction that converts Celsius to Fahrenheit.
+"""
 def temp(Cel):
+    """
+    Used the return statement to create the formula for converting celsius to fahrenheit.
+    """
     return Cel * 9/5 + 32
+"""
+Created a variable for a random celsius tempurate to be converted. Printed that variable.
+"""
 x = temp(14)
 print(x)
+
+# ------------------------------------------------------------
 
 """
 Task 6: Average of Numbers
@@ -75,13 +129,27 @@ and returns the average (mean) of those numbers.
 
 print()
 print("TASK 6")
+"""
+Created a function for calculating the average of numbers in the list below.
+"""
 def avg(y):
+    """
+    Used if statement to check if theres no numbers in the list to avoid division by zero error.
+    """
     if not y:
         return 0
+    """
+    Used the return statement to calculate the total sum of the numbers divided by how many numbers there are in the list.
+    """
     return sum(y) / len(y)
+"""
+Created a list of numbers to be calculated for the average. Finally printed the average.
+"""
 numbers = [5,2,7,5,9]
 average = avg(numbers)
 print(average)
+
+# ------------------------------------------------------------
 
 """
 Task 7: Total Calculator
@@ -91,10 +159,21 @@ and returns the total.
 
 print()
 print("TASK 7")
+"""
+Created a function to determine the price and quanity
+"""
 def totalprice(price, quantity):
+    """
+    Used return statement to calculate the total price by multiplying price and quantity.
+    """
     return price * quantity
+"""
+Gave a variable for the function with a price and quantity and then printed it out.
+"""
 x = totalprice(24, 5)
 print(f"${x}")
+
+# ------------------------------------------------------------
 
 """
 Your function should also optionally accept a 3rd argument for discount as a float, 
